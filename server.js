48d3627294a4245151956ab3387554d0fa63807d
ibrahim <ibrahim.laiq10@gmail.com>
2024-01-07 10:28:06 +0500

    error handler middleware

--- server.js
+++ server.js
@@ -1,7 +1,15 @@
 const express = require("express");
-const port = 5000;
+const dotenv = require("dotenv").config();
+const contactRoute = require("./routes/contactRoutes");
+const errorHandler = require("./middleware/errorHandler");
+const port = process.env.PORT || 4500;
 
 const app = express();
+
+app.use(express.json());
+
+app.use("/api/contacts", contactRoute);
+app.use(errorHandler);
 
 app.listen(port, () => {
   console.log(`App running on Port ${port}`);
